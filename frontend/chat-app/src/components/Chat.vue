ea254c931b3c59ea832c3c86fb942e640c3e7bd7
Miquel Bantulà <miki.bantula@gmail.com>
2020-04-21 19:52:34 +0200

    input + button to send test messages

--- frontend/chat-app/src/components/Chat.vue
+++ frontend/chat-app/src/components/Chat.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     {{ messages }}
+    <input type="text" v-model="newMessage" />
+    <button @click="sendNewMessage">Send</button>
   </div>
 </template>
 
@@ -8,9 +10,21 @@
 export default {
   name: "Chat",
 
+  data() {
+    return {
+      newMessage: ""
+    };
+  },
+
   props: {
     messages: {
-      type: Array,
+      type: Array
+    }
+  },
+
+  methods: {
+    sendNewMessage() {
+      this.$emit("messageSent", this.newMessage);
     }
   }
 };
