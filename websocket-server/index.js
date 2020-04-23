98346f6143b5ab9d4318d31d390b7cf84e5cd14e
Miquel Bantulà <miki.bantula@gmail.com>
2020-04-23 18:50:21 +0200

    trying to store participants serverside

--- websocket-server/index.js
+++ websocket-server/index.js
@@ -7,6 +7,7 @@ const SocketServer = require('ws').Server;
 const server = express().listen(3001);
 
 const wss = new SocketServer({ server });
+const users = [];
 
 wss.on('connection', (ws, request, client) => {
     console.log(`[Server] A client ${client} was connected with request ${request}.`);
@@ -14,8 +15,20 @@ wss.on('connection', (ws, request, client) => {
     ws.on('close', () => console.log('[Server] Client was disconnected.'));
 
     ws.on('message', (message) => {
-        console.log('[Server] Received message: %s', message);
+        const data = JSON.parse(message);
+        
+        if (data.type === 'user-connection') {
+            users.push(data.userName);
+            let message = {
+                type: 'activeUsers',
+                users: users
+            }
+            ws.send(JSON.stringify(message));
+        }
 
+        console.log('[Server] Received message: %s', data);
+
+        console.log('data.type', data.type);
         // we broadcast the message
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
