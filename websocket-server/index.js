7945426fbed2bcf9a8d0dd52f88edeb29f40c2b1
Miquel Bantulà <miki.bantula@gmail.com>
2020-04-25 09:12:54 +0200

    username validation serverside

--- websocket-server/index.js
+++ websocket-server/index.js
@@ -8,6 +8,7 @@ const server = express().listen(3001);
 
 const wss = new SocketServer({ server });
 const users = [];
+const messages = [];
 
 wss.on('connection', (ws, request, client) => {
     console.log(`[Server] A client ${client} was connected with request ${request}.`);
@@ -16,24 +17,55 @@ wss.on('connection', (ws, request, client) => {
 
     ws.on('message', (message) => {
         const data = JSON.parse(message);
-        
+
+
+        messages.push(data);
+        console.log('messages', messages);
+
         if (data.type === 'user-connection') {
-            users.push(data.userName);
-            let message = {
-                type: 'activeUsers',
-                users: users
+            if (users.includes(data.userName)) {
+                let m = {
+                    type: 'error',
+                    message: 'user name already exists!'
+                }
+                ws.send(JSON.stringify(m));
+            } else {
+                users.push(data.userName);
+                let m = {
+                    type: 'user-connection',
+                    message: `${data.userName} joined`,
+                    users: users
+                }
+                ws.send(JSON.stringify(m));
             }
-            ws.send(JSON.stringify(message));
         }
 
-        console.log('[Server] Received message: %s', data);
-
-        console.log('data.type', data.type);
-        // we broadcast the message
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
+        if (data.type === 'edit') {
+            let match = messages.find(message => message.id === data.id);
+            console.log('match', match);
+            console.log('message', message);
+            if (match) {
+                match.message = data.newMessage;
+                match.type = 'editted';
+                console.log('match2', match);
+                ws.send(JSON.stringify(match));
+            } else {
+                console.error('could not find the message to be editted');
             }
-        });
+        }
+
+        if (data.type === 'user-disconnection') {
+
+        }
+
+
+        if (data.type === 'message') {
+            // we broadcast the message
+            wss.clients.forEach(function each(client) {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(data));
+                }
+            });
+        }
     });
 });
